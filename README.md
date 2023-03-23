2e9ae920eea60ae7592a999785e1dd2e76cc6912
unknown <nduflot@dawan.fr>
2023-03-23 11:34:15 +0100

    dfd ajout d'option sur cEO et cTN + MAJ index + readme

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 * Attendre que le DOM soit chargé
 * Un querySelector qui renvoi une collection ou un seul élément, selon le selecteur (plus a choisir entre un selector ou selectorAll)
 * Un créateur d'élément DOM avec options pour inclure directement les attribut de l'élément.
+* Un créateur d'élément DOM
+* 
+    * avec options pour inclure directement les attribut de l'élément.
+    * l'élément créé peut directement être ajouter en tant qu'enfant à un élément parent en indiquant le selecteur du parent
 * D'autres encore qui sont en cuisine
 
 ### Usages
@@ -22,12 +26,15 @@ q fait un querySelectorAll du selecteur, ensuite analyse la collection.
 si la collection n'est qu'un élément, l'élément est retourné comme pour un querySelector,
 sinon retourne la collection comme un querySelectorAll
 
-#### cEO(element, options)
+#### cEO(element, options = {}, parent = null)
 Combine createElement et setAttribute.
-option est un objet litéral
+* option est un objet litéral
 * les noms des attribut de l'objet sont les noms des attributs de l'élément.
 * la valeur des attributs de l'objet sont les valeurs de l'attribut de l'élément
 * les attributs tels que ```class``` et ```style``` sont a renseigner en tableau
+* on peut indique en troisième attribut le sélecteur d'un élément parent dans lequel l'élément créé est ajouté
+* la fonction retourne par défaut l'élément créé
+
 ```
 options = {
     href: '#',
@@ -36,5 +43,6 @@ options = {
 }
 ```
 
-#### cTN(content)
-retourne simplement ```window.createTextNode(content)```
+#### cTN(content, parent = null)
+retourne un text node créé avec ```window.createTextNode(content)```
+Possibilité de mettre en second paramètre l'élément (en variable) parent dans lequel on ajoute le text node.
--- js/dfd-script.js
+++ js/dfd-script.js
@@ -1,41 +1,48 @@
 /**
  * DOM For Dummies
- * 
- * Petite bibliothèque de fonctions de raccourcis pour agir sur le DOM
+ * @author  Nicolas Duflot
+ * @since   17/03/2023
+ * @desc    Petite bibliothèque de fonctions de raccourcis pour agir sur le DOM
  * 
  */
 
+/**
+ * attendre que le DOM soit chargé
+ * @param {function} callable
+ */
 function loaded(callable){
-    /**
-     * attendre que le DOM soit chargé
-     */
     window.addEventListener('DOMContentLoaded', callable);
 }
-
+/**
+ * récupère un élément du DOM 
+ * ou 
+ * une collection d'élément du DOM
+ * dépend du selecteur
+ * @param {string} selector - selector for the html element(s)
+ */
 function q(selector){
-    /**
-     * récupère un élément du DOM ou une collection d'élément du DOM
-     * dépend du selecteur, la fonction évalue si c'est une collection de plusieurs éléments.
-     */
     let collection = document.querySelectorAll(selector);
     return (collection.length > 1)? collection : collection[0];
 }
-
-function cEO(element, options = {}){
-    /**
-     * crée un élément dans le DOM et ses attributs, classes et / ou style
-     * des classes ou des styles doivent être définies dans des tableaux dans l'objet options.
-     * 
-     * les clefs de l'objet options sont utilisées pour représenter l'attribut, 
-     * donc pour des attribut comme les dataset on écrira 'data-test': 'valeur'
-     * 
-     * Insérrer une classe
-     *      class: ['classe1', 'classe2', ... ]
-     * 
-     * Insérrer du style
-     *      style: ['property: value;', 'property: value;', ...]
-     * il faut ABSOLUMENT ajouter le ; à la fin d'une propriété CSS pour les classes.
-     */
+/**
+ * crée un élément dans le DOM et ses attributs, classes et / ou style
+ * des classes ou des styles doivent être définies dans des tableaux dans l'objet options.
+ * 
+ * les clefs de l'objet options sont utilisées pour représenter l'attribut, 
+ * donc pour des attribut comme les dataset on écrira 'data-test': 'valeur'
+ * 
+ * Insérrer une classe
+ *      class: ['classe1', 'classe2', ... ]
+ * 
+ * Insérrer du style
+ *      style: ['property: value;', 'property: value;', ...]
+ * il faut ABSOLUMENT ajouter le ; à la fin d'une propriété CSS pour les classes.
+ * 
+ * @param {string} element - tagname of the html element
+ * @param {Object} options - option for the html element attributes
+ */
+function cEO(element, options = {}, parent = null){
+    
     let newElement = document.createElement(element);
     for(let key in options){
         if(typeof options[key] === 'object'){
@@ -45,12 +52,20 @@ function cEO(element, options = {}){
             newElement.setAttribute(key, options[key]);
         }      
     }
+    if(null !== parent){
+        q(parent).appendChild(newElement)
+    }
     return newElement;
 }
 
-function cTN(content){
-    /**
-     * crée un noeud de text
-     */
-    return document.createTextNode(content);
+/**
+ * crée un noeud de text
+ * @param {string} content - text content of the text node
+ */
+function cTN(content, parent = null){
+    let textNode = document.createTextNode(content);
+    if(null !== parent){
+        parent.append(textNode);
+    }
+    return textNode;
 }
